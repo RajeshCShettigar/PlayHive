2036626ac0cba265e3048e4068f6f1cd278e1849
rajesh <rajeshshettigar249@gmail.com>
2024-01-05 14:38:55 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="bg-black max-h-dvh">
+      <h1 className="text-3xl font-bold underline text-white">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
